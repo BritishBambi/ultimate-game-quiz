671dc91392757e18c250d65663732132f56662e8
BritishBambi <josefjakubiak@gmail.com>
2021-12-06 20:02:20 +0000

    Add startGame function that hides welcome elements and shows the quiz on a click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,18 @@
-
 let playButton = document.getElementById("play-btn")
 
 playButton.addEventListener("click", startGame)
 
-function startGame(){
-    console.log("Game Started")
-    
+function startGame() {
+
+    let Welcome = document.getElementById("welcome")
+    let quizHeading = document.getElementById("quiz-heading")
+    let quizContent = document.getElementById("question-area")
+    let scores = document.getElementById("score-area")
+
+    console.log("Game Started");
+    playButton.classList.add("hidden");
+    Welcome.classList.add("hidden");
+    quizHeading.classList.remove("hidden");
+    quizContent.classList.remove("hidden");
+    scores.classList.remove("hidden");
 }
