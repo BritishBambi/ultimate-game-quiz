d31f7beb7ab2365a1e889e432eea1f76b8ee0e4c
BritishBambi <josefjakubiak@gmail.com>
2021-12-07 16:53:50 +0000

    Fill out questions with text and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,109 +6,109 @@ let shuffledQuestions, currentQuestionIndex
 // Array of questions that will be shuffled through and displayed after a question is answered.
 let questions = [{
 
-        question: "Question Text",
+        question: "In what year was Nintendo founded?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "1889 -",
+            2: "1989",
+            3: "1934",
+            4: "1962"
         }
     },
 
     {
-        question: "Question Text",
+        question: "What is the best selling videogame of all time?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "Tetris",
+            2: "Call of Duty",
+            3: "Minecraft -",
+            4: "Fortnite"
         }
     },
 
     {
-        question: "Question Text",
+        question: "What is the most expensive video game made to date?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
-        }
-    },
-
-    {
-
-        question: "Question Text",
-        answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "God of War",
+            2: "GTA V -",
+            3: "Call of Duty: Modern Warfare",
+            4: "Skyrim"
         }
     },
 
     {
 
-        question: "Question Text",
+        question: "How much did a virtual reality device cost in 1980?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "$49,000 -",
+            2: "$17,000",
+            3: "$62,000",
+            4: "$32,000"
         }
     },
 
     {
 
-        question: "Question Text",
+        question: "What position did the creator of the Game Boy have at Nintendo?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "CFO",
+            2: "Intern",
+            3: "Video Game Designer",
+            4: "Janitor-"
         }
     },
 
     {
 
-        question: "Question Text",
+        question: "Mario first appeared in what video game?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "Mario Kart",
+            2: "Super Mario Brothers",
+            3: "Donkey Kong -",
+            4: "Super Mario Baseball"
         }
     },
 
     {
 
-        question: "Question Text",
+        question: "What was the first video game to be played in outer space?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "StarCraft -",
+            2: "Pong",
+            3: "World of Warcraft",
+            4: "Tetris"
         }
     },
 
     {
 
-        question: "Question Text",
+        question: "What world market generates the most revenue in the video game industry?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "England",
+            2: "China -",
+            3: "Japan",
+            4: "United States"
         }
     },
 
     {
 
-        question: "Question Text",
+        question: "What is the highest-grossing movie based on a video game?",
         answer: {
-            1: "Answer1",
-            2: "Answer2",
-            3: "Answer3",
-            4: "Answer4"
+            1: "Doom",
+            2: "Resident Evil: Afterlife",
+            3: "Tomb Raider",
+            4: "Detective Pikachu -"
+        }
+    },
+
+    {
+
+        question: "What is the highest-selling gaming console to date?",
+        answer: {
+            1: "Nintendo Switch",
+            2: "Playstation 2 -",
+            3: "Xbox 360",
+            4: "Nintendo Wii"
         }
     },
 ]
